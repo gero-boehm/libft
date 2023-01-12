1bf564bf7a6cab847f6048561e2a581cd5acbfdb
Gero Bohm <gbohm@1-B-25.42heilbronn.de>
2023-01-12 16:59:28 +0100

    added extstr2

---
+++ ft_extstr2.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_extstr2.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbohm <gbohm@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/12 16:51:32 by gbohm             #+#    #+#             */
+/*   Updated: 2023/01/12 16:59:14 by gbohm            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_extstr2(char **str, size_t amount)
+{
+	size_t	len;
+	char	*new_str;
+
+	len = ft_strlen(*str);
+	if (amount <= len)
+		return (0);
+	if (ft_calloc2(amount + 1, sizeof(char), &new_str))
+		return (1);
+	while (len--)
+		new_str[len] = (*str)[len];
+	free(*str);
+	*str = new_str;
+	return (0);
+}
