bc08b1c7d2a292ddd463d800f2d383d1ae3c7ae4
Gero Bohm <gbohm@1-B-25.42heilbronn.de>
2023-01-12 17:33:08 +0100

    fixed bug

--- ft_extstr2.c
+++ ft_extstr2.c
@@ -6,7 +6,7 @@
 /*   By: gbohm <gbohm@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 16:51:32 by gbohm             #+#    #+#             */
-/*   Updated: 2023/01/12 17:29:19 by gbohm            ###   ########.fr       */
+/*   Updated: 2023/01/12 17:33:04 by gbohm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_extstr2(char **str, size_t amount)
 	len = ft_strlen(*str);
 	if (amount <= len)
 		return (0);
-	if (ft_calloc2(amount + 1, sizeof(char), &new_str))
+	if (ft_calloc2(amount + 1, sizeof(char), (void **) &new_str))
 		return (1);
 	while (len--)
 		new_str[len] = (*str)[len];
